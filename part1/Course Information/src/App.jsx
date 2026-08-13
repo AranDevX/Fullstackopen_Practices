cc94934a8a434d154e4ddbf4d05c1e6f8c2949de
AranDevX <aranarasmus@gmail.com>
2026-08-13 21:27:20 +0330

    forgot to also fix Total Component it's fixed now.

--- part1/Course Information/src/App.jsx
+++ part1/Course Information/src/App.jsx
@@ -25,7 +25,7 @@ const Total = (props) => {
   console.log(props)
   return (
     <div>
-      <p> Number of exercises {props.content[0].exercises+props.content[1].exercises+props.content[2].exercises}</p>
+      <p> Number of exercises {props.exercises1+props.exercises2+props.exercises3}</p>
     </div>
   )
 }

4cba71167c737dcf2743d3e462ba3c2750a08261
AranDevX <aranarasmus@gmail.com>
2026-08-13 20:33:35 +0330

    it seems like I accidentally fixed 1.4 for exercise 1.2 so I fixed it back to 1.2

--- part1/Course Information/src/App.jsx
+++ part1/Course Information/src/App.jsx
@@ -15,9 +15,9 @@ const Content = (props) => {
   console.log(props)
   return (
     <div>
-      <Part part={props.content[0].part} exercises={props.content[0].exercises} />
-      <Part part={props.content[1].part} exercises={props.content[1].exercises} />
-      <Part part={props.content[2].part} exercises={props.content[2].exercises} />
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </div>
   )
 }
@@ -31,17 +31,24 @@ const Total = (props) => {
 }
 const App = () => {
   const course = 'Half Stack application development'
-  const content = [
-    {part : 'Fundamentals of React',exercises : 10},
-    {part : 'Using props to pass data',exercises : 7},
-    {part : 'State of a component',exercises : 14},
-  ]
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
     <div>
       <Header course={course} />
-      <Content content={content} />
-      <Total content={content} />
+      <Content 
+        part1={part1} exercises1={exercises1}
+        part2={part2} exercises2={exercises2}
+        part3={part3} exercises3={exercises3}/>
+      <Total 
+        exercises1={exercises1} 
+        exercises2={exercises2} 
+        exercises3={exercises3} />
     </div>
   )
 }
